ad3cf7f5f9cd00c6bb2e217d52708e9192caad56
Didier Verna <didier@didierverna.net>
2016-12-26 18:48:02 +0100

    Bump version number.

--- _define.php
+++ _define.php
@@ -26,7 +26,7 @@ $this->registerModule
 ('noCAPTCHA',
  __('Protect the comment submission form with a noCAPTCHA'),
  'Didier Verna',
- '1.0-b11',
+ '1.0-b12',
  array ('type'        => 'plugin',
 	'permissions' => 'admin'));
 
