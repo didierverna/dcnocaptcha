b9a967520713415965b0e9a13b011822811e8b18
Didier Verna <didier@didierverna.net>
2016-12-27 12:11:50 +0100

    Version 1.0.

--- _define.php
+++ _define.php
@@ -26,7 +26,7 @@ $this->registerModule
 ('noCAPTCHA',
  __('Protect the comment submission form with a noCAPTCHA'),
  'Didier Verna',
- '1.0-b15',
+ '1.0',
  array ('type'        => 'plugin',
 	'permissions' => 'admin'));
 
